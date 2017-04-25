2717781b7e57f788f9eb71268c566b5ba0a964ac
Adrian Abreu <aabreuglez@gmail.com>
2017-04-25 17:38:15 +0100

    Added parser improvement to the demo

--- src/demo.ts
+++ src/demo.ts
@@ -4,21 +4,24 @@ import { Superescalar } from './core/Superescalar';
 declare var document;
 declare var window;
 
-function load() {
+function load(id) {
 
-   let input = document.getElementById('input_code');
-   console.log('Input', input.value);
+   let input = document.getElementById(id);
    let code: Code = new Code();
-   let superescalar: Superescalar = new Superescalar();
-   superescalar.code = code;
-   code.load(input.value);
-   let lines = document.getElementById('lines');
-   let numberOfBlocks = document.getElementById('numberOfBlocks');
-   let basicBlocks = document.getElementById('basicBlocks');
-   let instructions = document.getElementById('instructions');
-   lines.innerText = 'Lines: ' + code.lines;
-   numberOfBlocks.innerText = 'Number of blocks: ' + code.numberOfBlocks;
-   basicBlocks.innerText = 'Basic Blocks \n' + JSON.stringify(code.basicBlocks);
-   instructions.innerText = 'Instructions \n' + JSON.stringify(code.instructions);
+   // let superescalar: Superescalar = new Superescalar();
+   // superescalar.code = code;
+   try {
+      code.load(input.value);
+   } catch (err) {
+      window.alert(err);
+   }
+   // let lines = document.getElementById('lines');
+   // let numberOfBlocks = document.getElementById('numberOfBlocks');
+   // let basicBlocks = document.getElementById('basicBlocks');
+   // let instructions = document.getElementById('instructions');
+   // lines.innerText = 'Lines: ' + code.lines;
+   // numberOfBlocks.innerText = 'Number of blocks: ' + code.numberOfBlocks;
+   // basicBlocks.innerText = 'Basic Blocks \n' + JSON.stringify(code.basicBlocks);
+   // instructions.innerText = 'Instructions \n' + JSON.stringify(code.instructions);
 }
 window.load = load;
