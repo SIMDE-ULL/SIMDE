94a3384c52414c8079f85c512e864fe3520f2cd6
Adrian Abreu <aabreuglez@gmail.com>
2017-05-27 20:08:31 +0100

    Added lodash to the project

--- src/interface/tab/RegistersTabComponent.tsx
+++ src/interface/tab/RegistersTabComponent.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { RegisterComponent } from '../RegisterComponent';
-
+import * as _ from 'lodash';
 
 export class RegisterTabComponent extends React.Component<any, any> {
 
