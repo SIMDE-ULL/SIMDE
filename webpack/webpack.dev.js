6218a805adb0ea3c9955b804f6fbf2814239e09e
Adrian Abreu <me@adrianabreu.com>
2017-06-27 21:14:11 +0000

    Added plugin for copy translations to target

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,32 +4,36 @@ const commonConfig = require('./webpack.common.js');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const webpackMerge = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 const ENV = 'dev';
 
 
 module.exports = webpackMerge(commonConfig({ env: ENV }), {
-   devServer: {
-      contentBase: './target/www'
-   },
-   devtool: "source-map",
-   output: {
-      path: path.resolve('target/www'),
-      filename: '[name].bundle.js',
-      chunkFilename: '[id].chunk.js'
-   },
-   plugins: [
-      new BrowserSyncPlugin({
-         host: 'localhost',
-         port: 9000,
-         proxy: {
-            target: 'http://localhost:9060'
-         }
-      }, {
-            reload: true
-         }),
-      new HtmlWebpackPlugin({
-         title: 'Sinde DEMO',
-         template: 'src/index.html'
-      }),
-   ]
+      devServer: {
+            contentBase: './target/www'
+      },
+      devtool: "source-map",
+      output: {
+            path: path.resolve('target/www'),
+            filename: '[name].bundle.js',
+            chunkFilename: '[id].chunk.js'
+      },
+      plugins: [
+            new BrowserSyncPlugin({
+                  host: 'localhost',
+                  port: 9000,
+                  proxy: {
+                        target: 'http://localhost:9060'
+                  }
+            }, {
+                        reload: true
+                  }),
+            new HtmlWebpackPlugin({
+                  title: 'Sinde DEMO',
+                  template: 'src/index.html'
+            }),
+            new CopyWebpackPlugin([
+                  { from: 'src/i18n' }
+            ])
+      ]
 });
