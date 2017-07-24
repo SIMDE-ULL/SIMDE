89e5de85dee85611de424b4af92f84c9541b7ac9
Adrian Abreu <me@adrianabreu.com>
2017-07-24 19:56:48 +0200

    Hope this is the last capitalize fix

--- src/core/Common/Memory.ts
+++ src/core/Common/Memory.ts
@@ -1,78 +1,78 @@
-import { randomNumber } from '../utils/Random';
+import { randomNumber } from '../Utils/Random';
 
 export interface Datum {
-   datum: number;
-   got: boolean;
+    datum: number;
+    got: boolean;
 }
 
 export class Memory {
 
-   private static MEMORY_NUMBER: number = 1024;
+    private static MEMORY_NUMBER: number = 1024;
 
-   private _data: number[];
-   private _fail: boolean[];
-   private _failProbability: number;
+    private _data: number[];
+    private _fail: boolean[];
+    private _failProbability: number;
 
-   constructor() {
-      this.data = new Array(Memory.MEMORY_NUMBER);
-      this.fail = new Array(Memory.MEMORY_NUMBER);
+    constructor() {
+        this.data = new Array(Memory.MEMORY_NUMBER);
+        this.fail = new Array(Memory.MEMORY_NUMBER);
 
-      this.failProbability = 0;
-   }
+        this.failProbability = 0;
+    }
 
-   getDatum(address: number): Datum {
-      if (address < 0) {
-         address = 0;
-      }
-      let valueToReturn = {
-         datum: this.data[address],
-         got: true
-      };
-      let failValue = randomNumber(100);
+    getDatum(address: number): Datum {
+        if (address < 0) {
+            address = 0;
+        }
+        let valueToReturn = {
+            datum: this.data[address],
+            got: true
+        };
+        let failValue = randomNumber(100);
 
-      // There will be a fail only if there wasn't a previous fail on the same position
-      if ((failValue < this.failProbability) && !this.fail[address]) {
-         this.fail[address] = true;
-         valueToReturn.got = false;
-         return valueToReturn;
-      }
-      this.fail[address] = true;
-      return valueToReturn;
-   }
+        // There will be a fail only if there wasn't a previous fail on the same position
+        if ((failValue < this.failProbability) && !this.fail[address]) {
+            this.fail[address] = true;
+            valueToReturn.got = false;
+            return valueToReturn;
+        }
+        this.fail[address] = true;
+        return valueToReturn;
+    }
 
-   setDatum(address: number, value: number) {
-      if (address < 0) {
-         address = 0;
-      }
-      this.data[address] = value;
-   }
+    setDatum(address: number, value: number) {
+        if (address < 0) {
+            address = 0;
+        }
+        this.data[address] = value;
+    }
 
-   setMem(datum: number) {
-      this.data.fill(datum);
-   }
+    setMem(datum: number) {
+        this.data.fill(datum);
+    }
 
-   public get data(): number[] {
-      return this._data;
-   }
+    public get data(): number[] {
+        return this._data;
+    }
 
-   public set data(value: number[]) {
-      this._data = value;
-   }
+    public set data(value: number[]) {
+        this._data = value;
+    }
 
-   public get fail(): boolean[] {
-      return this._fail;
-   }
+    public get fail(): boolean[] {
+        return this._fail;
+    }
 
-   public set fail(value: boolean[]) {
-      this._fail = value;
-   }
+    public set fail(value: boolean[]) {
+        this._fail = value;
+    }
 
-   public get failProbability(): number {
-      return this._failProbability;
-   }
+    public get failProbability(): number {
+        return this._failProbability;
+    }
 
-   public set failProbability(value: number) {
-      this._failProbability = value;
-   }
+    public set failProbability(value: number) {
+        this._failProbability = value;
+    }
 
 }
