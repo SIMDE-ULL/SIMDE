19e73f9ea5ea0978b560d90784d015ec74fe5e0d
Adrian Abreu <me@adrianabreu.com>
2018-02-27 23:04:55 +0000

    Fixed first tab layout

--- src/interface/components/navbar/FileBarComponent.tsx
+++ src/interface/components/navbar/FileBarComponent.tsx
@@ -17,57 +17,58 @@ class FileBarComponent extends React.Component<any, any> {
     }
 
     render() {
-        return (<div className='filebar'>
+        return (<div className='smd-filebar'>
             <nav className='navbar'>
                 <ul className='nav navbar-nav'>
-                <DropdownButton
-                    title={t('fileBar.file.name')}
-                    key={'dropdown-load'}
-                    id={'dropdown-load'}
-                >
-                    <MenuItem eventKey="1" onClick={() => { this.props.actions.toggleLoadModal(true) }}>{t('fileBar.file.load')}</MenuItem>
-                </DropdownButton>
+                    <DropdownButton
+                        title={t('fileBar.file.name')}
+                        key={'dropdown-load'}
+                        id={'dropdown-load'}
+                        noCaret
+                    >
+                        <MenuItem eventKey="1" onClick={() => { this.props.actions.toggleLoadModal(true) }}>{t('fileBar.file.load')}</MenuItem>
+                    </DropdownButton>
                 </ul>
                 <ul className='nav navbar-nav'>
-                    <li className='dropdown'>
-                        <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>{t('fileBar.view.name')}</a>
-                        <ul className='dropdown-menu'>
-                            <li><a onClick={() => {
-                                this.color = !this.color;
-                                window['colorBlocks'](this.color);
-                            }}>{t('fileBar.view.basicBlocks')}</a></li>
-                        </ul>
-                    </li>
+                    <DropdownButton
+                        title={t('fileBar.view.name')}
+                        key={'dropdown-view'}
+                        id={'dropdown-view'}
+                        noCaret
+                    >
+                        <MenuItem eventKey="1" onClick={() => {
+                            this.color = !this.color;
+                            window['colorBlocks'](this.color);
+                        }}>{t('fileBar.view.basicBlocks')}</MenuItem>
+                    </DropdownButton>
                 </ul>
                 <ul className='nav navbar-nav'>
-                    <li className='dropdown'>
-                        <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>{t('fileBar.config.name')}</a>
-                        <ul className='dropdown-menu'>
-                            <li><a onClick={() => { window['superConfigModal'](true); }}>{t('fileBar.config.superescalar')}</a></li>
-                            <li><a onClick={() => { window['options'](true); }}>{t('fileBar.config.options')}</a></li>
-                        </ul>
-                    </li>
+                    <DropdownButton
+                        title={t('fileBar.config.name')}
+                        key={'dropdown-options'}
+                        id={'dropdown-options'}
+                        noCaret
+                    >
+                        <MenuItem eventKey="1" onClick={() => { window['superConfigModal'](true); }}>{t('fileBar.config.superescalar')}</MenuItem>
+
+                        <MenuItem eventKey="2" onClick={() => { window['options'](true); }}>{t('fileBar.config.options')}</MenuItem>
+                    </DropdownButton>
                 </ul>
                 <ul className='nav navbar-nav'>
-                    <li className='dropdown'>
-                        <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>{t('fileBar.execute.name')}</a>
-                        <ul className='dropdown-menu'>
-                            <li onClick={() => window.play()}><a>{t('fileBar.execute.init')}</a></li>
-                            <li onClick={() => window.pause()}><a>{t('fileBar.execute.pause')}</a></li>
-                            <li onClick={() => window.stop()}><a>{t('fileBar.execute.stop')}</a></li>
-                            <li onClick={() => window.stepBack()}><a>{t('fileBar.execute.back')}</a></li>
-                            <li onClick={() => window.superStep()}><a>{t('fileBar.execute.forward')}</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <ul className='nav navbar-nav'>
-                    <li className='dropdown'>
-                        <a className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>{t('fileBar.help.name')}</a>
-                        <ul className='dropdown-menu'>
-                            <li><a href='http://adrianabreu.com/SIMDE-Docs/' target='_blank'>{t('fileBar.help.docs')}</a></li>
-                            <li><a onClick={() => { window['autorModal'](true); }}>{t('fileBar.help.about')}</a></li>
-                        </ul>
-                    </li>
+                    <DropdownButton
+                        title={t('fileBar.help.name')}
+                        key={'dropdown-help'}
+                        id={'dropdown-help'}
+                        noCaret
+                    >
+                        <MenuItem eventKey="1">
+                            {
+                                // <a href="https://etsiiull.github.io/SIMDE/">{t('fileBar.help.docs')}</a>
+                            }
+                        </MenuItem>
+
+                        <MenuItem eventKey="2" onClick={() => { window['autorModal'](true); }}>{t('fileBar.help.about')}</MenuItem>
+                    </DropdownButton>
                 </ul>
             </nav>
         </div>);
@@ -81,8 +82,8 @@ const mapStateToProps = state => {
 }
 
 function mapDispatchToProps(dispatch) {
-    return { actions: bindActionCreators({toggleLoadModal}, dispatch)};
+    return { actions: bindActionCreators({ toggleLoadModal }, dispatch) };
 }
 
 
-export default translate('common', { wait: true })(connect(mapStateToProps,mapDispatchToProps)(FileBarComponent));
+export default translate('common', { wait: true })(connect(mapStateToProps, mapDispatchToProps)(FileBarComponent));
--- src/interface/components/tab/GeneralTabComponent.tsx
+++ src/interface/components/tab/GeneralTabComponent.tsx
@@ -20,92 +20,66 @@ class GeneralTabComponent extends React.Component<any, any> {
     }
 
     render() {
-        return (<div id='home'>
-            <div className='row'>
-                <div className='col-sm-3' id='code-zone'>
-                    <div className='row'>
-                        <CodeComponent code={this.props.code}/>
-                    </div>
+        return (
+            <div className="smd-general_tab">
+                <div className="smd-general_tab-code">
+                    <CodeComponent code={this.props.code} />
                 </div>
-                <div className='col-sm-9' id='simulation-zone'>
-                    <div className='row'>
-                        <div className='col-sm-5'>
-                            <div className='row'>
-                                <div className='col-sm-6'>
-                                    <div className='row'>
-                                        <PrefetchDecoderComponent data={this.props.prefetchUnit} title='Prefetch' />
-                                    </div>
-                                </div>
-                                <div className='col-sm-6'>
-                                    <div className='row'>
-                                        <PrefetchDecoderComponent data={this.props.decoder} title='Decoder' />
-                                    </div>
-                                </div>
+                <div className="smd-general_tab-simulation">
+                    <div className='smd-general_tab-simulation_left'>
+                        <div className="smd-general_tab-simulation_prefetch_decoder">
+                            <div className='w-50'>
+                                <PrefetchDecoderComponent data={this.props.prefetchUnit} title='Prefetch' />
                             </div>
-                            <div className='row'>
-                                <div className='col-sm-4'>
-                                    <div className='row register-mapper'>
-                                        <ROBMapperComponent title='ROB<->GPR' data={this.props.ROBGpr.content} />
-                                    </div>
-                                </div>
-                                <div className='col-sm-4'>
-                                    <div className='row register-mapper'>
-                                        <ROBMapperComponent title='ROB<->FPR' data={this.props.ROBFpr.content} />
-                                    </div>
-                                </div>
-                                <div className='col-sm-4'>
-                                    <div className='row register-mapper'>
-                                        <JumpPredictionComponent jumpPrediction={this.props.jumpPrediction} title='Jump table' />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className='row'>
-                                <ReorderBufferComponent title='ReorderBuffer' content={this.props.reorderBuffer} />
+                            <div className='w-50'>
+                                <PrefetchDecoderComponent data={this.props.decoder} title='Decoder' />
                             </div>
                         </div>
-                        <div className='col-sm-5'>
-                            <div className='row'>
-                                <div className='col-sm-12'>
-                                    <div className='row'>
-                                        <div className='panel panel-default inside-bar' id='reserve-station-zone'>
-                                            <div className='panel-heading'>{t('Reserve Stations')}</div>
-                                            <div className='panel-body'>
-                                                <ReserveStationComponent title='Integer +' data={this.props.reserveStationIntAdd}/>
-                                                <ReserveStationComponent title='Integer x' data={this.props.reserveStationIntSub}/>
-                                                <ReserveStationComponent title='Floating +' data={this.props.reserveStationFloAdd}/>
-                                                <ReserveStationComponent title='Floating x' data={this.props.reserveStationFloSub}/>
-                                                <ReserveStationComponent title='Memory' data={this.props.reserveStationMemory}/>
-                                                <ReserveStationComponent title='Jump' data={this.props.reserveStationJump}/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div className="smd-general_tab-simulation_mappers">
+                            <div className='smd-general_tab-simulation_register_mapper'>
+                                <ROBMapperComponent title='ROB<->GPR' data={this.props.ROBGpr.content} />
+                            </div>
+                            <div className='smd-general_tab-simulation_register_mapper'>
+                                <ROBMapperComponent title='ROB<->FPR' data={this.props.ROBFpr.content} />
+                            </div>
+                            <div className='smd-general_tab-simulation_register_mapper'>
+                                <JumpPredictionComponent title='Jump table' jumpPrediction={this.props.jumpPrediction} />
                             </div>
                         </div>
-                        <div className='col-sm-2'>
-                            <div className='row'>
-                                <div className='col-sm-12'>
-                                    <div className='row'>
-                                        <div className='panel panel-default inside-bar' id='functional-unit-zone'>
-                                            <div className='panel-heading'>{t('UF')}</div>
-                                            <div className='panel-body'>
-                                                <FunctionalUnitComponent title='+Entera' header={this.props.functionalUnitIntAdd.header} content={this.props.functionalUnitIntAdd.content} />
-                                                <FunctionalUnitComponent title='xEntera' header={this.props.functionalUnitIntSub.header} content={this.props.functionalUnitIntSub.content} />
-                                                <FunctionalUnitComponent title='+Flotante' header={this.props.functionalUnitFloAdd.header} content={this.props.functionalUnitFloAdd.content} />
-                                                <FunctionalUnitComponent title='xFlotante' header={this.props.functionalUnitFloSub.header} content={this.props.functionalUnitFloSub.content} />
-                                                <FunctionalUnitComponent title='Mem' header={this.props.functionalUnitMemory.header} content={this.props.functionalUnitMemory.content} />
-                                                <FunctionalUnitComponent title='JumpUF' header={this.props.functionalUnitJump.header} content={this.props.functionalUnitJump.content} />
-                                                <FunctionalUnitComponent title='AluMem' header={this.props.functionalUnitAluMem.header} content={this.props.functionalUnitAluMem.content} />
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
+                        <div className="smd-general_tab-simulation_reorder_buffer">
+                            <ReorderBufferComponent title='ReorderBuffer' content={this.props.reorderBuffer} />
+                        </div>
+                    </div>
+                    <div className='smd-general_tab-simulation_center'>
+                        <div className='panel panel-default inside-bar' id='reserve-station-zone'>
+                            <div className='panel-heading'>{t('Reserve Stations')}</div>
+                            <div className='panel-body'>
+                                <ReserveStationComponent title='Integer +' data={this.props.reserveStationIntAdd} />
+                                <ReserveStationComponent title='Integer x' data={this.props.reserveStationIntSub} />
+                                <ReserveStationComponent title='Floating +' data={this.props.reserveStationFloAdd} />
+                                <ReserveStationComponent title='Floating x' data={this.props.reserveStationFloSub} />
+                                <ReserveStationComponent title='Memory' data={this.props.reserveStationMemory} />
+                                <ReserveStationComponent title='Jump' data={this.props.reserveStationJump} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='smd-general_tab-simulation_right'>
+                        <div className='panel panel-default inside-bar' id='functional-unit-zone'>
+                            <div className='panel-heading'>{t('UF')}</div>
+                            <div className='panel-body'>
+                                <FunctionalUnitComponent title='+Entera' header={this.props.functionalUnitIntAdd.header} content={this.props.functionalUnitIntAdd.content} />
+                                <FunctionalUnitComponent title='xEntera' header={this.props.functionalUnitIntSub.header} content={this.props.functionalUnitIntSub.content} />
+                                <FunctionalUnitComponent title='+Flotante' header={this.props.functionalUnitFloAdd.header} content={this.props.functionalUnitFloAdd.content} />
+                                <FunctionalUnitComponent title='xFlotante' header={this.props.functionalUnitFloSub.header} content={this.props.functionalUnitFloSub.content} />
+                                <FunctionalUnitComponent title='Mem' header={this.props.functionalUnitMemory.header} content={this.props.functionalUnitMemory.content} />
+                                <FunctionalUnitComponent title='JumpUF' header={this.props.functionalUnitJump.header} content={this.props.functionalUnitJump.content} />
+                                <FunctionalUnitComponent title='AluMem' header={this.props.functionalUnitAluMem.header} content={this.props.functionalUnitAluMem.content} />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>);
+        );
     }
 }
 
