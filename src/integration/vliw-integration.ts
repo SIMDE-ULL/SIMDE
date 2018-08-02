af6761edbbb5877c223b7896f542577a8b487f9b
alu0100884012 <alu0100884012@ull.edu.es>
2018-08-02 18:57:41 +0100

    creating vliw integration

---
+++ src/integration/vliw-integration.ts
@@ -0,0 +1,221 @@
+import { Superescalar } from '../core/Superescalar/Superescalar';
+import { ExecutionStatus } from '../main-consts';
+import { store } from '../store';
+import {
+    nextPrefetchCycle,
+    nextDecoderCycle,
+    nextJumpTableCycle,
+    nextFunctionalUnitCycle,
+    nextReserveStationCycle,
+    nextReorderBufferCycle,
+    nextRegistersCycle,
+    nextMemoryCycle,
+    nextReorderBufferMapperCycle,
+    nextCycle,
+    superescalarLoad,
+    batchActions,
+    colorCell
+} from '../interface/actions';
+
+import { pushHistory, takeHistory, resetHistory } from '../interface/actions/history';
+import { MAX_HISTORY_SIZE } from '../interface/reducers';
+
+import { t } from 'i18next';
+import { Code } from '../core/Common/Code';
+import { SuperescalarStatus } from '../core/Superescalar/SuperescalarEnums';
+import { displayBatchResults } from '../interface/actions/modals';
+
+import { MachineIntegration } from './machine-integration';
+import { VLIW } from '../core/VLIW/VLIW';
+
+export class VLIWIntegration extends MachineIntegration {
+    // Global objects for binding React to the View
+    vliw = new VLIW();
+    codeLoaded = false;
+    interval = null;
+    backStep = 0;
+    stopCondition = ExecutionStatus.EXECUTABLE;
+    finishedExecution = false;
+    executing = false;
+    replications = 0;
+    cacheFailPercentage = 0;
+    cacheFailLatency = 0;
+
+    /*
+    * This call all the components to update the state
+    * if there is a step param, the components will use
+    * their history to set the appropiate content
+    */
+    dispatchAllVLIWActions = (step?: number) => {
+        // Code should only be setted on the first iteration
+        store.dispatch(
+                batchActions(
+                    nextFunctionalUnitCycle([...this.vliw.functionalUnit, this.vliw.aluMem]),
+                    nextRegistersCycle([this.vliw.gpr.content, this.vliw.fpr.content]),
+                    nextMemoryCycle(this.vliw.memory.data),
+                    nextCycle(this.vliw.status.cycle),
+                )
+        );
+    }
+
+    vliwExe = () => {
+        this.vliw.init(true);
+    }
+
+    stepForward = () => {
+
+        if (!this.vliw.code) {
+            return;
+        }
+
+        // if (this.backStep > 0) {
+        //     this.backStep--;
+        //     store.dispatch(takeHistory(this.backStep));
+        // } else {
+            if (this.finishedExecution) {
+                this.finishedExecution = false;
+                this.resetMachine();
+            }
+            if (this.vliw.status.cycle === 0) {
+                let code = Object.assign(new Code(), this.vliw.code);
+                this.superExe();
+                this.vliw.code = code;
+            }
+            let machineStatus = this.vliw.tic();
+            this.dispatchAllVLIWActions();
+
+            return machineStatus;
+        // }
+    }
+
+    loadCode = (code: Code) => {
+        this.superescalar.code = code;
+        this.resetMachine();
+        // There is no need to update the code with the rest,
+        // it should remain the same during all the program execution
+        store.dispatch(superescalarLoad(code.instructions));
+    }
+
+    play = () => {
+
+        if (!this.superescalar.code) {
+            return;
+        }
+
+        this.stopCondition = ExecutionStatus.EXECUTABLE;
+        this.backStep = 0;
+        this.executing = true;
+        let speed = this.calculateSpeed();
+
+        // Check if the execution has finished
+        if (this.finishedExecution) {
+            this.finishedExecution = false;
+            this.resetMachine();
+        }
+
+        if (this.superescalar.status.cycle === 0) {
+            let code = Object.assign(new Code(), this.superescalar.code);
+            this.superExe();
+            this.vliw.code = code;
+        }
+
+        if (speed) {
+            this.executionLoop(speed);
+        } else {
+            // tslint:disable-next-line:no-empty
+            while (this.vliw.tic() !== VLIWStatus.SUPER_ENDEXE) { }
+            this.dispatchAllSuperescalarActions();
+            this.finishedExecution = true;
+            alert(t('execution.finished'));
+        }
+    }
+
+    makeBatchExecution = () => {
+        if (!this.vliw.code) {
+            return;
+        }
+
+        const results = [];
+        for (let i = 0; i < this.replications; i++) {
+            let code = Object.assign(new Code(), this.superescalar.code);
+            this.superExe();
+            this.vliw.code = code;
+            this.vliw.memory.failProbability = this.cacheFailPercentage;
+            this.superescalar.memoryFailLatency = this.cacheFailLatency;
+
+            // tslint:disable-next-line:no-empty
+            while (this.superescalar.tic() !== SuperescalarStatus.SUPER_ENDEXE) { }
+            results.push(this.superescalar.status.cycle);
+        }
+
+        const statistics = this.calculateBatchStatistics(results);
+        this.clearBatchStateEffects();
+        store.dispatch(displayBatchResults(statistics));
+    }
+
+    pause = () => {
+        this.stopCondition = ExecutionStatus.PAUSE;
+        this.executing = false;
+    }
+
+    stop = () => {
+        if (!this.superescalar.code) {
+            return;
+        }
+        // In normal execution I have to avoid the asynchrnous way of
+        // js entering in the interval, the only way I have is to using a semaphore
+        this.stopCondition = ExecutionStatus.STOP;
+
+        if (!this.executing) {
+            this.executing = false;
+            this.resetMachine();
+        }
+    }
+
+
+    stepBack = () => {
+        // // There is no time travelling for batch mode and initial mode
+        // if (this.superescalar.status.cycle > 0 && this.backStep < MAX_HISTORY_SIZE &&
+        //     (this.superescalar.status.cycle - this.backStep > 0)) {
+        //     this.backStep++;
+        //     store.dispatch(takeHistory(this.backStep));
+        // }
+    }
+
+    executionLoop = (speed) => {
+        if (!this.stopCondition) {
+            setTimeout(() => {
+                let machineStatus = this.stepForward();
+                if (!(machineStatus === SuperescalarStatus.SUPER_BREAKPOINT || machineStatus === SuperescalarStatus.SUPER_ENDEXE)) {
+                    this.executionLoop(speed);
+                } else {
+                    if (machineStatus === SuperescalarStatus.SUPER_BREAKPOINT) {
+                        alert(t('execution.stopped'));
+                    } else if (machineStatus === SuperescalarStatus.SUPER_ENDEXE) {
+                        this.finishedExecution = true;
+                        alert(t('execution.finished'));
+                    }
+                }
+            }, speed);
+        } else if (this.stopCondition === ExecutionStatus.STOP) {
+            this.resetMachine();
+        }
+    }
+
+
+    // setBatchMode = (replications: number, cacheFailLatency, cacheFailPercentage) => {
+    //     this.replications = replications;
+    //     this.cacheFailLatency = cacheFailLatency;
+    //     this.cacheFailPercentage = cacheFailPercentage;
+    // }
+
+    private resetMachine() {
+        let code = Object.assign(new Code(), this.vliw.code);
+        this.vliwExe();
+        this.vliw.code = code;
+        this.dispatchAllVLIWActions();
+        store.dispatch(resetHistory());
+    }
+}
+
+export default new VLIWIntegration();
