a4625b20fe0cf59ef2414d769685c69560b50b56
Adrian Abreu <aabreu@countergrogwise.com>
2021-08-27 16:40:41 +0200

    Using basename

--- src/interface/App.tsx
+++ src/interface/App.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import { createBrowserHistory } from "history";
-
 import { translate } from "react-i18next";
 import LandingPageComponent from "./components/LandingPage/LandingPageComponent";
 import projectpage from "./components/LandingPage/ProjectPageComponent";
@@ -12,9 +10,8 @@ import { VLIWComponent } from "./components/VLIW/VLIWComponent";
 
 class App extends React.Component<any, any> {
    render() {
-      const history = createBrowserHistory({ basename: '/SIMDE' });
       return (
-          <Router history={history} >
+          <Router basename={"/SIMDE"} >
               <Structure />
           </Router>
      );
@@ -23,7 +20,7 @@ class App extends React.Component<any, any> {
 
 const Structure = () => (
     <div className="pagebody">
-      <Route path="/" component={LandingPageComponent} />
+      <Route exact path="/" component={LandingPageComponent} />
       <Route path="/Superescalar" component={SuperescalarComponent} />
       <Route path="/VLIW" component={VLIWComponent} />
       <Route path="/Project" component={projectpage} />
