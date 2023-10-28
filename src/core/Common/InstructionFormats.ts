bb3fdd3283f60ac1fbbbe43069c84fa807df385a
endes <endes@disroot.org>
2023-10-28 13:39:15 +0100

    comments

--- src/core/Common/InstructionFormats.ts
+++ src/core/Common/InstructionFormats.ts
@@ -2,13 +2,13 @@ import { Opcodes } from './Opcodes';
 
 
 export enum Formats {
-    TwoGeneralRegisters = 0,
-    TwoFloatingRegisters,
-    GeneralRegisterAndInmediate,
-    GeneralLoadStore,
-    FloatingLoadStore,
-    Jump,
-    NooP
+    TwoGeneralRegisters = 0,        // OP R1, R2, R3
+    TwoFloatingRegisters,           // OP F1, F2, F3
+    GeneralRegisterAndInmediate,    // OP R1, R2, #X
+    GeneralLoadStore,               // OP R1, X(R2)
+    FloatingLoadStore,              // OP F1, X(R2)
+    Jump,                           // OP R1, R2, label
+    NooP                            // NOP
 }
 
 export let FormatsNames: string[] = ["TwoGeneralRegisters", "TwoFloatingRegisters", "GeneralRegisterAndInmediate", "GeneralLoadStore", "FloatingLoadStore", "Jump", "NooP"];
