fa47413b0841601e80cff10be1af50e0ccde1dfa
endes <endes@disroot.org>
2023-11-12 17:42:45 +0000

    Finished newop and shift tests

--- src/test/functional/Superescalar/newop.spec.ts
+++ src/test/functional/Superescalar/newop.spec.ts
@@ -19,8 +19,17 @@ test('nuevaOp.pla is executed properly', t => {
     t.context.machine.code = t.context.code;
     while (t.context.machine.tic() !== SuperescalarStatus.SUPER_ENDEXE) { }
 
+    // Check registers
+    t.deepEqual(t.context.machine.getGpr(2), 3, 'R2 is not 3');
+    t.deepEqual(t.context.machine.getGpr(3), 2, 'R3 is not 2');
+    t.deepEqual(t.context.machine.getGpr(4),-1, 'R4 is not -1');
+    t.deepEqual(t.context.machine.getGpr(5), 0, 'R5 is not 0 (has been modified by the branch)');
+    t.deepEqual(t.context.machine.getGpr(6), 1, 'R6 is not 1');
+
     // Check where the program counter is
-    //t.deepEqual(t.context.machine.status.cycle, 212, 'Bad pc at finish');
-    t.deepEqual(1, 1, 'Bad pc at finish');
+    t.deepEqual(t.context.machine.pc, 7, 'Bad pc at finish');
+
+    // Check the number of cycles are correct
+    t.deepEqual(t.context.machine.status.cycle, 13, 'Bad number of cycles at finish');
 
 })
--- src/test/functional/Superescalar/shift.spec.ts
+++ src/test/functional/Superescalar/shift.spec.ts
@@ -19,8 +19,21 @@ test('despl.pla is executed properly', t => {
     t.context.machine.code = t.context.code;
     while (t.context.machine.tic() !== SuperescalarStatus.SUPER_ENDEXE) { }
 
-    // Check where the program counter is
-    //t.deepEqual(t.context.machine.status.cycle, 212, 'Bad pc at finish');
-    t.deepEqual(1, 1, 'Bad pc at finish');
+    // Check registers
+    t.deepEqual(t.context.machine.getGpr(1), 3, 'R1 is not 3');
+    t.deepEqual(t.context.machine.getGpr(2), 2, 'R2 is not 2');
+    t.deepEqual(t.context.machine.getGpr(3), 12, 'R3 is not 12, wrong shift left');
+    t.deepEqual(t.context.machine.getGpr(4), 0, 'R4 is not 0, wrong shift right');
+    t.deepEqual(t.context.machine.getGpr(5), 11, 'R5 is not 11');
+    t.deepEqual(t.context.machine.getGpr(6), 6, 'R6 is not 6');
+    t.deepEqual(t.context.machine.getGpr(7), 15, 'R7 is not 15, wrong or');
+    t.deepEqual(t.context.machine.getGpr(8), 2, 'R8 is not 2, wrong and');
+    t.deepEqual(t.context.machine.getGpr(9), -16, 'R9 is not -12, wrong nor');
+    t.deepEqual(t.context.machine.getGpr(10), 13, 'R10 is not 13, wrong xor');
 
+    // Check where the program counter is
+    t.deepEqual(t.context.machine.pc, 10, 'Bad pc at finish');
+
+    // Check the number of cycles are correct
+    t.deepEqual(t.context.machine.status.cycle, 10, 'Bad number of cycles at finish');
 })
