7bdc45353f8c8e0950dc5a00d3d31085a42e5860
Óscar Carrasco <me@oxca.be>
2024-04-07 22:18:44 +0100

    chore: implement development shell in flake.nix

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,14 @@
 # syntax=docker/dockerfile:1.7
 
 # Nix builder
-FROM nixos/nix:latest AS builder
+FROM nixos/nix-flakes:latest AS builder
 
 # Copy our source and setup our working dir
 COPY . /tmp/build
 WORKDIR /tmp/build
 
 # Build our Nix environment
-RUN nix \
-    --extra-experimental-features "nix-command flakes" \
-    --option filter-syscalls false \
-    build
+RUN nix build
 
 # Copy the Nix store closure into a directory
 RUN mkdir /tmp/nix-store-closure
--- flake.nix
+++ flake.nix
@@ -1,37 +1,88 @@
+# ============================================================================ #
+#
+# Educational computer simulator on a mission to "superscalate" the study of
+# computer architecture fundamentals.
+#
+# ---------------------------------------------------------------------------- #
 {
-  description = "simde";
+  description = "Educational computer architecture simulator";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     pnpm2nix = {
       url = "github:nzbr/pnpm2nix-nzbr";
-			inputs.nixpkgs.follows = "nixpkgs";
-      };
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, pnpm2nix, ... } @ inputs:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in with pkgs; rec {
-        # Development environment
-        devShell = mkShell {
-          name = "simde";
-          nativeBuildInputs = [ nodejs nodePackages.pnpm ];
-        };
+  outputs =
+    { self
+    , nixpkgs
+    , pre-commit-hooks
+    , pnpm2nix
+    , ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
 
-        # Build package into dist/ dir with pnpm
-        dist = pnpm2nix.packages.${system}.mkPnpmPackage {
-          src = ./.;
+      # Helper to provide system-specific attributes
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs systems (system:
+          f rec {
+            pkgs = nixpkgs.legacyPackages.${system};
+            commonPackages = builtins.attrValues {
+              inherit (pkgs) nodejs;
+              inherit (pkgs.nodePackages) pnpm;
+            };
+          });
+    in
+    {
+      pre-commit-check = pre-commit-hooks.run {
+        src = builtins.path { path = ./.; };
+        default_stages = [ "manual" "push" ];
+        hooks = {
+          nixpkgs-fmt.enable = true;
+          commitizen.enable = true;
+          biome = {
+            enable = true;
+            name = "Biome.js";
+            entry = "biome check --apply";
+            stages = [ "pre-push" ];
+          };
         };
+        settings = {
+          alejandra.verbosity = "quiet";
+        };
+      };
 
-        # Create SIMDE dist/ + SWS bundle
-        packages.app = pkgs.writeShellScriptBin "app" ''
-          ${pkgs.static-web-server}/bin/static-web-server --root ${dist}.distDir
+      # Development environment
+      devShells = forAllSystems ({ pkgs, commonPackages }: {
+        inherit (self.pre-commit-check) shellHook;
+        default = pkgs.mkShell {
+          packages = with pkgs; [ nixpkgs-fmt biome ] ++ commonPackages;
+        };
+      });
+
+      # Build package into `dist/` dir from `pnpm-lock.yaml`
+      buildDist = pnpm2nix.mkPnpmPackage {
+        src = ./.;
+        noDevDependencies = true;
+      };
+
+      # Create SIMDE `dist/` + `Static-Web-Server` bundle
+      packages = forAllSystems ({ pkgs, ... }: {
+        default = pkgs.writeShellScriptBin "default" ''
+          ${pkgs.static-web-server}/bin/static-web-server --root .
         '';
-
-        # The default package when a specific package name isn't specified
-        defaultPackage = packages.app;
-      }
-    );
+      });
+    };
 }
