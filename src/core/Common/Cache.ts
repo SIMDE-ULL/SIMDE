3501c4f2089a6c1921fba1905c47febac1221e2a
endes <endes@disroot.org>
2024-04-09 17:29:47 +0100

    feat(core): implement direct mapped cache

--- src/core/Common/Cache.ts
+++ src/core/Common/Cache.ts
@@ -41,3 +41,48 @@ export class RandomCache extends NoCache {
     return this.memory.setData(address, value);
   }
 }
+
+export class DirectCache extends NoCache {
+  private readonly blocks_tags: number[];
+  private block_size: number;
+
+  constructor(
+    memory: Memory,
+    private _blocks: number, // number of blocks
+    private _size: number
+  ) {
+    super(memory);
+
+    // Calculate the block size
+    this.block_size = Math.floor(_size / _blocks);
+
+    // Initializa the blocks tags to -1
+    this.blocks_tags = Array(_blocks).fill(-1);
+  }
+
+  public getFaultyDatum(address: number): Datum | Error {
+    const data = this.memory.getData(address);
+
+    // get the tag of the address
+    const tag = Math.floor(address / this.block_size);
+
+    // check if the tag is in the cache
+    const faultOccurred = this.blocks_tags[tag % this._blocks] !== tag;
+
+    // set the tag in the cache
+    this.blocks_tags[tag % this._blocks] = tag;
+
+    if (data instanceof Error) return data;
+    return { value: data, got: faultOccurred };
+  }
+
+  public setDatum(address: number, value: number): undefined | Error {
+    // get the tag of the address
+    const tag = Math.floor(address / this.block_size);
+
+    // set the tag in the cache
+    this.blocks_tags[tag % this._blocks] = tag;
+
+    return this.memory.setData(address, value);
+  }
+}
