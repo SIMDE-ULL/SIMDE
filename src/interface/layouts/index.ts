e2eb3037a0ca1a4170235d71efdf7752e38d3c05
Óscar Carrasco <me@oxca.be>
2025-03-10 18:58:10 +0000

    feat: create Simulator layout

---
+++ src/interface/layouts/index.ts
@@ -0,0 +1,4 @@
+import SimulatorLayout from "./Simulator/Simulator";
+import type { SimulatorLayoutProps } from "./Simulator/Simulator.types";
+
+export { SimulatorLayout, type SimulatorLayoutProps };
