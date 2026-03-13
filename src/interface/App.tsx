9d83365a077066392ccb78b2b1f15ddc36374a9d
Óscar Carrasco <me@oxca.be>
2026-03-13 19:27:24 +0000

    refactor(imports): replace react-router-dom with react-router
    Update all imports from react-router-dom to react-router.
    Remove unused BrowserRouter and Route imports from LandingPageComponent.

--- src/interface/App.tsx
+++ src/interface/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import LandingPageComponent from "./components/LandingPage/LandingPageComponent";
 import ProjectPage from "./components/LandingPage/ProjectPageComponent";
 
--- src/interface/components/LandingPage/LandingPageComponent.tsx
+++ src/interface/components/LandingPage/LandingPageComponent.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import { Link } from 'react-router';
 // import projectpage from "./ProjectPageComponent";  // Unused?
 import { useTranslation } from "react-i18next";
 
