6aae188beedb1c41f38e2f857b871a7a97d1528f
Óscar Carrasco <me@oxca.be>
2026-03-16 11:44:44 +0000

    chore(deps): replace normalize.css CDN with npm package, remove @popperjs/core
    Bundle normalize.css via @use instead of CDN link in root.tsx.
    Remove unused @popperjs/core dependency. Fix typedoc docs script.

--- src/root.tsx
+++ src/root.tsx
@@ -26,10 +26,6 @@ export function Layout({ children }: { children: ReactNode }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
         <title>SIMDE</title>
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-        />
         <Meta />
         <Links />
       </head>
